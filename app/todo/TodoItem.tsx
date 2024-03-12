32b6249f637af7f4b156fc12b6d2f29888312f85
azu <azuciao@gmail.com>
2024-03-13 08:57:06 +0900

    loading

--- app/todo/TodoItem.tsx
+++ app/todo/TodoItem.tsx
@@ -1,13 +1,27 @@
 "use client"
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { useSearchParams } from "next/navigation";
-export const TodoItemWithQuery = () => {
+import { Suspense } from "react";
+
+/*
+    <Suspense fallback={<div>Require Params</div>}>
+        <TodoItemWithLoadingSearchParams>
+          <TodoItem id={id} />
+        </TodoItemWithLoadingSearchParams>
+    </Suspense>
+
+ */
+const TodoPlaceholder = () => {
+    return <div>Loading...</div>
+
+}
+export const TodoItemWithLoadingSearchParams = () => {
     const searchParams = useSearchParams();
     const id = searchParams.get("id");
-    if (!id) {
-        return <div>Invalid ID</div>
-    }
-    return <TodoItem id={id}/>
+    if (!id) return <TodoPlaceholder/>;
+    return <Suspense fallback={<TodoPlaceholder/>}>
+        <TodoItem id={id}/>
+    </Suspense>
 }
 export const TodoItem = (props: { id: string; }) => {
     const { data } = useSuspenseQuery({
--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -1,13 +1,13 @@
 import { Counter } from "./Counter";
 import { Suspense } from "react";
-import { TodoItemWithQuery } from "./TodoItem";
+import { TodoItemWithLoadingSearchParams } from "./TodoItem";
 
 export const dynamic = "force-static";
 export default function Page() {
     return (
         <div>
-            <Suspense fallback={<div>Loading...</div>}>
-                <TodoItemWithQuery />
+            <Suspense fallback={<div>Require Params</div>}>
+                <TodoItemWithLoadingSearchParams />
             </Suspense>
             <Counter/>
         </div>
