533810e28ea986f6ca4796065a72d1f912c86148
azu <azuciao@gmail.com>
2024-03-13 01:44:41 +0900

    fix

--- app/todo/TodoItem.tsx
+++ app/todo/TodoItem.tsx
@@ -5,7 +5,7 @@ const fetcher = (url: string) => fetch(url).then((res) => res.json());
 export const TodoItem = (props: { id: string; }) => {
     const { data } = useSuspenseQuery({
         queryKey: ["todo", props.id],
-        queryFn: () => fetcher(`https://jsonplaceholder.typicode.com/todo/${props.id}`),
+        queryFn: () => fetcher(`https://jsonplaceholder.typicode.com/todos/${props.id}`),
     });
     return <div>todo: {data.title}</div>
 }
