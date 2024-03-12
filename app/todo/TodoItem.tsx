f1d41891fd5dd41aa9b1f7eb2ab486ee3e544181
azu <azuciao@gmail.com>
2024-03-13 08:29:54 +0900

    use wrapper

--- app/todo/TodoItem.tsx
+++ app/todo/TodoItem.tsx
@@ -1,11 +1,18 @@
 "use client"
 import { useSuspenseQuery } from "@tanstack/react-query";
-
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
+import { useSearchParams } from "next/navigation";
+export const TodoItemWithQuery = () => {
+    const searchParams = useSearchParams();
+    const id = searchParams.get("id");
+    if (!id) {
+        return <div>Invalid ID</div>
+    }
+    return <TodoItem id={id}/>
+}
 export const TodoItem = (props: { id: string; }) => {
     const { data } = useSuspenseQuery({
         queryKey: ["todo", props.id],
-        queryFn: () => fetcher(`https://jsonplaceholder.typicode.com/todos/${props.id}`),
+        queryFn: () => fetch(`https://jsonplaceholder.typicode.com/todos/${props.id}`).then((res) => res.json())
     });
     return <div>todo: {data.title}</div>
 }
--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -1,16 +1,13 @@
 import { Counter } from "./Counter";
 import { Suspense } from "react";
-import { TodoItem } from "./TodoItem";
+import { TodoItemWithQuery } from "./TodoItem";
 
 export const dynamic = "force-static";
-
-
-export default function Page({ searchParams }: { searchParams: { id: string } }) {
-    const { id } = searchParams
+export default function Page() {
     return (
         <div>
             <Suspense fallback={<div>Loading...</div>}>
-                <TodoItem id={id}/>
+                <TodoItemWithQuery />
             </Suspense>
             <Counter/>
         </div>
